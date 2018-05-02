c485c263882e783bd30f957cb0ed014ecb5f55f4
Dima <rudoydmitry@yandex.ru>
2018-05-02 20:57:38 +0300

    a

---
+++ courseWORK/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using courseWORK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace courseWORK.Controllers
+{
+    public class AccountController : Controller
+    {
+   
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                User user = null;
+                using (SudokuDBEntities db = new SudokuDBEntities())
+                {
+                    user = db.User.FirstOrDefault(u => u.Email == model.Name && u.Password == model.Password);
+
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = null;
+                using (SudokuDBEntities db = new SudokuDBEntities())
+                {
+                    user = db.User.FirstOrDefault(u => u.Email == model.Name);
+                }
+                if (user == null)
+                {
+                    // создаем нового пользователя
+                    using (SudokuDBEntities db = new SudokuDBEntities())
+                    {
+                        db.User.Add(new User { Email = model.Name, Password = model.Password });
+                        db.SaveChanges();
+
+                        user = db.User.Where(u => u.Email == model.Name && u.Password == model.Password).FirstOrDefault();
+                    }
+                    // если пользователь удачно добавлен в бд
+                    if (user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Name, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
+                }
+            }
+
+            return View(model);
+        }
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ courseWORK/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace courseWORK.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public JsonResult As()
+        {
+            string k = "as";
+            return Json(k, JsonRequestBehavior.AllowGet);
+        }
+        public string Andex()
+        {
+            string result = "Вы не авторизованы";
+            if (User.Identity.IsAuthenticated)
+            {
+                result = "Ваш логин: " + User.Identity.Name;
+            }
+            return result;
+        }
+    }
+}
